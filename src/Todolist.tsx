7e918bb3dc4e0f7994e9af6ae428cea9442c393d
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-05-24 16:48:27 +0300

    move Todolist.tsx to src, remove Todolist folder

---
+++ src/Todolist.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+type TodolistPropsType = {
+    title?: string | number
+    tasks: Array<TasksPropsType>
+}
+
+type TasksPropsType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+export const Todolist = (props: TodolistPropsType) => {
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                {props.tasks.map(el => {
+                    return (
+                        <li><input type="checkbox" checked={el.isDone}/> <span>{el.title}</span></li>
+                    )
+                })}
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    );
+}
