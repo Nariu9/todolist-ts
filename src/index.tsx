883c546381450c180d93171badca62b289256c64
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-08-01 22:11:15 +0300

    import all App components and pass Redux store to App using Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+// import AppWithReducers from './AppWithReducers';
+// import AppWithRedux from './AppWithRedux';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from 'react-redux';
+import {store} from './state/store';
+import AppWithReduxUpgraded from './AppWithReduxUpgraded';
 
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}>
+    <AppWithReduxUpgraded/>
+</Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
