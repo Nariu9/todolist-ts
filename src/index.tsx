c1c667080792878f8159ea9f5aba7fb7ada7144f
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-08 23:25:46 +0300

    wrap App with BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,15 @@ import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux';
 import {store} from './app/store';
 import App from './app/App';
+import {BrowserRouter} from 'react-router-dom';
 
 
-ReactDOM.render(<Provider store={store}>
-    <App/>
-</Provider>, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
