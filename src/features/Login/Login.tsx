7b3793e99f6bfc3a80445d6686c6a5d01a5bb3e2
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-09 12:30:26 +0300

    create Login component

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -7,12 +7,56 @@ import FormGroup from '@mui/material/FormGroup';
 import FormLabel from '@mui/material/FormLabel';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import {useFormik} from 'formik';
+import {useAppDispatch, useAppSelector} from '../../app/hooks';
+import {loginTC} from './auth-reducer';
+import {Navigate} from 'react-router-dom';
+
+type FormikErrorType = {
+    email?: string
+    password?: string
+    rememberMe?: boolean
+}
 
 export const Login = () => {
+    const dispatch = useAppDispatch()
+    const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false
+        },
+        validate: (values) => {
+            const errors: FormikErrorType = {}
+            if (!values.email) {
+                errors.email = 'Email required';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+            }
+
+            if (!values.password) {
+                errors.password = 'Password required';
+            } else if (values.password.length < 8) {
+                errors.password = 'Password must be 8 characters or more';
+            }
+            return errors
+        },
+        onSubmit: values => {
+            dispatch(loginTC(values))
+            formik.resetForm()
+        },
+    })
+
+    if (isLoggedIn) {
+        return <Navigate to={'/todolist-ts'}/>
+    }
+
     return <Grid container justifyContent={'center'}>
         <Grid item justifyContent={'center'}>
             <FormControl>
-                <FormLabel>
+                <FormLabel style={{color: 'inherit'}}>
                     <p>To log in get registered
                         <a href={'https://social-network.samuraijs.com/'}
                            target={'_blank'} rel={'noreferrer'}> here
@@ -22,22 +66,35 @@ export const Login = () => {
                     <p>Email: free@samuraijs.com</p>
                     <p>Password: free</p>
                 </FormLabel>
-                <FormGroup>
 
-                    <TextField label="Email"
-                               margin="normal"
-                    />
+                <form onSubmit={formik.handleSubmit}>
+                    <FormGroup>
 
-                    <TextField type="password"
-                               label="Password"
-                               margin="normal"
-                    />
+                        <TextField label="Email"
+                                   margin="normal"
+                                   {...formik.getFieldProps('email')}
+                        />
+                        {formik.touched.email && formik.errors.email &&
+                            <div style={{color: 'red'}}>{formik.errors.email}</div>}
 
-                    <FormControlLabel label={'Remember me'} control={<Checkbox/>}/>
-                    <Button type={'submit'} variant={'contained'} color={'primary'}>
-                        Login
-                    </Button>
-                </FormGroup>
+                        <TextField type="password"
+                                   label="Password"
+                                   margin="normal"
+                                   {...formik.getFieldProps('password')}
+                        />
+                        {formik.touched.password && formik.errors.password &&
+                            <div style={{color: 'red'}}>{formik.errors.password}</div>}
+
+                        <FormControlLabel label={'Remember me'}
+                                          control={<Checkbox
+                                              checked={formik.values.rememberMe}
+                                              {...formik.getFieldProps('rememberMe')}
+                                          />}/>
+                        <Button type={'submit'} variant={'contained'} color={'primary'}>
+                            Login
+                        </Button>
+                    </FormGroup>
+                </form>
             </FormControl>
         </Grid>
     </Grid>
