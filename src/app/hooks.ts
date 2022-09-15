d269d89b720484c6052a319ac2d543f5ac29ee00
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-15 22:58:31 +0300

    rename AppRootStateType to RootState

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,6 +1,6 @@
-import {AppDispatch, AppRootStateType} from './store';
+import {AppDispatch, RootState} from './store';
 import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
 export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
