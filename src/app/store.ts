ba3197e38d51f1af417dd9d71fb201ab34c114e6
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-15 22:57:42 +0300

    replace createStore with configureStore, temporarily comment out unused code

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,15 +1,16 @@
-import {AnyAction, applyMiddleware, combineReducers, compose, legacy_createStore as createStore} from 'redux';
-import {TasksActionsType, tasksReducer} from '../features/TodolistsList/tasks-reducer';
-import {TodolistsActionsType, todolistsReducer} from '../features/TodolistsList/todolists-reducer';
-import {AppActionsType, appReducer} from './app-reducer';
-import thunk, {ThunkAction, ThunkDispatch} from 'redux-thunk';
+import {AnyAction, combineReducers} from 'redux';
+import {tasksReducer} from '../features/TodolistsList/tasks-reducer';
+import {todolistsReducer} from '../features/TodolistsList/todolists-reducer';
+import {appReducer} from './app-reducer';
+import {ThunkAction, ThunkDispatch} from 'redux-thunk';
 import {authReducer} from '../features/Login/auth-reducer';
+import {configureStore} from '@reduxjs/toolkit';
 
-declare global {
-    interface Window {
-        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-    }
-}
+// declare global {
+//     interface Window {
+//         __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+//     }
+// }
 
 const rootReducer = combineReducers({
     tasks: tasksReducer,
@@ -18,19 +19,23 @@ const rootReducer = combineReducers({
     auth: authReducer
 })
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
-
-export type AppRootStateType = ReturnType<typeof rootReducer>
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+export const store = configureStore({
+    reducer: rootReducer,
+    /*middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunk),
+    devTools: true*/
+})
+export type RootState = ReturnType<typeof rootReducer>
 // export type RootState = ReturnType<typeof store.getState>
 
 
-type StoreActionsType = TodolistsActionsType | TasksActionsType | AppActionsType
+// type StoreActionsType = TodolistsActionsType | TasksActionsType | AppActionsType
 
 
-export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, StoreActionsType>
+export type AppDispatch = ThunkDispatch<RootState, unknown, AnyAction>
 
-export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AnyAction>
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, AnyAction>
 
 // @ts-ignore
 window.store = store
