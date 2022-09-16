51edf5427f78e6691c6efaf4be662cadf7bedcad
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-16 10:01:50 +0300

    delete commented code from store.ts

--- src/app/store.ts
+++ src/app/store.ts
@@ -6,12 +6,6 @@ import {ThunkAction, ThunkDispatch} from 'redux-thunk';
 import {authReducer} from '../features/Login/auth-reducer';
 import {configureStore} from '@reduxjs/toolkit';
 
-// declare global {
-//     interface Window {
-//         __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-//     }
-// }
-
 const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer,
@@ -19,19 +13,10 @@ const rootReducer = combineReducers({
     auth: authReducer
 })
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 export const store = configureStore({
     reducer: rootReducer,
-    /*middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunk),
-    devTools: true*/
 })
 export type RootState = ReturnType<typeof rootReducer>
-// export type RootState = ReturnType<typeof store.getState>
-
-
-// type StoreActionsType = TodolistsActionsType | TasksActionsType | AppActionsType
-
 
 export type AppDispatch = ThunkDispatch<RootState, unknown, AnyAction>
 
