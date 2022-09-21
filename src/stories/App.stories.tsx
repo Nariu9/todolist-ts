18b642b3fec675723909a512d6c865b7e317237a
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-21 10:02:58 +0300

    add HashRouterDecorator to App stories decorators

--- src/stories/App.stories.tsx
+++ src/stories/App.stories.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
 import App from '../app/App';
-import {ReduxStoreProviderDecorator} from './decorators/ReduxStoreProviderDecorator';
+import {HashRouterDecorator, ReduxStoreProviderDecorator} from './decorators/ReduxStoreProviderDecorator';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Todolists/App',
-  component: App,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  decorators: [ReduxStoreProviderDecorator]
+    title: 'Todolists/App',
+    component: App,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    decorators: [ReduxStoreProviderDecorator, HashRouterDecorator]
 } as ComponentMeta<typeof App>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
@@ -17,5 +17,5 @@ const Template: ComponentStory<typeof App> = (args) => <App {...args}/>;
 export const AppStory = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 AppStory.args = {
-  demo: true
+    demo: true
 }
