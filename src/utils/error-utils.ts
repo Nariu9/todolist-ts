e2b96f9ea7f25a14c23799707b548424b3819902
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-29 23:55:09 +0300

    pass ThunkDispatch type to util functions

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,13 +1,15 @@
 import {AppDispatch} from '../app/store';
 import {setAppErrorAC, setAppStatusAC} from '../app/app-reducer';
 import {ResponseType} from '../api/todolists-api';
+import {ThunkDispatch} from 'redux-thunk';
+import {AnyAction} from 'redux';
 
-export const handleServerNetworkError = (error: { message: string }, dispatch: AppDispatch) => {
+export const handleServerNetworkError = (error: { message: string }, dispatch: AppDispatch | ThunkDispatch<unknown, unknown, AnyAction>) => {
     dispatch(setAppStatusAC({status: 'failed'}))
     dispatch(setAppErrorAC({error: error.message ? error.message : 'Some error occurred'}))
 }
 
-export const handleServerAppError = <T>(data: ResponseType<T>, dispatch: AppDispatch) => {
+export const handleServerAppError = <T>(data: ResponseType<T>, dispatch: AppDispatch |  ThunkDispatch<unknown, unknown, AnyAction>) => {
     if (data.messages.length) {
         dispatch(setAppErrorAC({error: data.messages[0]}))
     } else {
