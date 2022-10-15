47ed96bfcb294620874d68b1957a87d7878b0d87
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-10-15 21:06:30 +0300

    implement useActions hook

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,6 +1,16 @@
-import {AppDispatch, RootState} from './store';
+import {AppDispatch, RootStateType} from './store';
 import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
+import {ActionCreatorsMapObject, bindActionCreators} from 'redux';
+import {useMemo} from 'react';
 
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
+
 export const useAppDispatch: () => AppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useAppSelector: TypedUseSelectorHook<RootStateType> = useSelector
+
+export function useActions<T extends ActionCreatorsMapObject<any>>(actions: T) {
+    const dispatch = useAppDispatch()
+
+    return useMemo(() => {
+        return bindActionCreators(actions, dispatch)
+    }, [actions, dispatch])
+}
