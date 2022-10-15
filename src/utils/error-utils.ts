2f0f6ac85e5b54326ce5c4ddcc3d0a2940357b0e
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-10-15 21:30:08 +0300

    add some commented code

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,19 +1,22 @@
 import {AppDispatch} from '../app/store';
-import {setAppErrorAC, setAppStatusAC} from '../app/app-reducer';
+import {setAppError, setAppStatus} from '../app/app-reducer';
 import {ResponseType} from '../api/todolists-api';
 import {ThunkDispatch} from 'redux-thunk';
 import {AnyAction} from 'redux';
+// import {appActions} from '../app';
+
+// const {setAppStatus, setAppError} = appActions
 
 export const handleServerNetworkError = (error: { message: string }, dispatch: AppDispatch | ThunkDispatch<unknown, unknown, AnyAction>) => {
-    dispatch(setAppStatusAC({status: 'failed'}))
-    dispatch(setAppErrorAC({error: error.message ? error.message : 'Some error occurred'}))
+    dispatch(setAppStatus({status: 'failed'}))
+    dispatch(setAppError({error: error.message ? error.message : 'Some error occurred'}))
 }
 
 export const handleServerAppError = <T>(data: ResponseType<T>, dispatch: AppDispatch |  ThunkDispatch<unknown, unknown, AnyAction>) => {
     if (data.messages.length) {
-        dispatch(setAppErrorAC({error: data.messages[0]}))
+        dispatch(setAppError({error: data.messages[0]}))
     } else {
-        dispatch(setAppErrorAC({error: 'Some error occurred'}))
+        dispatch(setAppError({error: 'Some error occurred'}))
     }
-    dispatch(setAppStatusAC({status: 'failed'}))
+    dispatch(setAppStatus({status: 'failed'}))
 }
