c26d39458e54d8adabf9999cee6fcb6c65a64c2a
Artem Karpukhin <a.karpukhin9@gmail.com>
2023-01-12 20:17:14 +0200

    implement app sagas

---
+++ src/app/app-sagas.ts
@@ -0,0 +1,23 @@
+import {AxiosResponse} from 'axios';
+import {authAPI, ResponseType, ResultCodes} from '../api/todolists-api';
+import {call, put, takeEvery} from 'redux-saga/effects';
+import {setLoggedInAC} from '../features/Login/auth-reducer';
+import {setAppInitializedAC} from './app-reducer';
+
+//action creators for sagas
+export const initializeApp = () => ({type: 'APP/INITIALIZE-APP'})
+
+//sagas
+export function* initializeAppWorkerSaga() {
+    const res: AxiosResponse<ResponseType<{ id: number }>> = yield call(authAPI.me)
+    if (res.data.resultCode === ResultCodes.successfully) {
+        yield put(setLoggedInAC(true))
+    } else {
+
+    }
+    yield put(setAppInitializedAC(true))
+}
+
+export function*  appWatcherSaga () {
+    yield takeEvery('APP/INITIALIZE-APP', initializeAppWorkerSaga);
+}
