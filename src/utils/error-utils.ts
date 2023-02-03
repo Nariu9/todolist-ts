7df6f425704f1764e315927c58776b634d0235f7
Artem Karpukhin <a.karpukhin9@gmail.com>
2023-02-03 22:17:41 +0200

    implement util functions as generators

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,6 +1,7 @@
 import {AppDispatch} from '../app/store';
 import {setAppErrorAC, setAppStatusAC} from '../app/app-reducer';
 import {ResponseType} from '../api/todolists-api';
+import {put} from 'redux-saga/effects';
 
 export const handleServerNetworkError = (error: { message: string }, dispatch: AppDispatch) => {
     dispatch(setAppStatusAC('failed'))
@@ -14,4 +15,18 @@ export const handleServerAppError = <T>(data: ResponseType<T>, dispatch: AppDisp
         dispatch(setAppErrorAC('Some error occurred'))
     }
     dispatch(setAppStatusAC('failed'))
+}
+
+export function* handleServerAppErrorSaga<T>(data: ResponseType<T>) {
+    if (data.messages.length) {
+        yield put(setAppErrorAC(data.messages[0]))
+    } else {
+        yield put(setAppErrorAC('Some error occurred'))
+    }
+    yield put(setAppStatusAC('failed'))
+}
+
+export function* handleServerNetworkErrorSaga(error: { message: string }) {
+    yield put(setAppStatusAC('failed'))
+    yield put(setAppErrorAC(error.message ? error.message : 'Some error occurred'))
 }
